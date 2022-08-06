ab96c39f6a4e4a5572889f970b8aa610f6cac44f
stasfr <stas240600@yandex.ru>
2022-08-06 03:23:40 +0300

    get data from parent

--- src/components/v-catalog-item.vue
+++ src/components/v-catalog-item.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="v-catalog-item">
-    <p>Item 1</p>
-    <p>Price 100</p>
-    <button>add to cart</button>
+    <img class="v-catalog-item__image" :src="require('../assets/images/' + productData.image)" :alt="productData.image">
+    <div class="v-catalog-item__name">{{productData.name}}</div>
+    <div class="v-catalog-item__price">{{productData.price}} &#8381;</div>
+    <button class="v-catalog-item__add_to_card_btn btn">Buy</button>
   </div>
 </template>
 
