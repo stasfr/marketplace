40d239d9d7b9cf4535dbdba220af66d5348b10b6
stasfr <stas240600@yandex.ru>
2022-08-06 04:04:16 +0300

    added vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,34 @@
+import axios from 'axios'
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
+  state: {
+    products: []
+  },
+  getters: {
+    PRODUCTS(state) {
+      return state.products
+    }
+  },
+  mutations: {
+    SET_PRODUCTS_TO_STATE: (state, products) => {
+      state.products = products
+    }
+  },
+  actions: {
+    GET_PRODUCTS_FROM_API({ commit }) {
+      return axios('http://localhost:3000/products', {
+        method: 'GET'
+      })
+        .then(products => {
+          commit('SET_PRODUCTS_TO_STATE', products.data)
+          return products
+        })
+        .catch(error => {
+          console.log(error)
+          return error
+        })
+    }
+  },
   modules: {}
 })
