135c106ff480b99b00ea0ac9546f86dded4a86a8
stasfr <stas240600@yandex.ru>
2022-08-06 19:20:11 +0300

    added cart info, styles

--- src/components/v-cart-item.vue
+++ src/components/v-cart-item.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="v-cart-item">
-    <p>{{productCartData.name}}</p>
+    <img
+      class="v-cart-item__image"
+      :src="require('../assets/images/' + productCartData.image)"
+      :alt="productCartData.image"
+    />
+    <div class="v-cart-item__info">
+      <div>{{ productCartData.name }}</div>
+      <div>{{ productCartData.article }}</div>
+      <div>{{ productCartData.price }} &#8381;</div>
+    </div>
+    <button class="v-cart-item__btn btn">Remove</button>
   </div>
 </template>
 
