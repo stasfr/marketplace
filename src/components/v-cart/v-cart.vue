e43a471ada7b02a569349db846dc7b8e363b0cd7
stasfr <stas240600@yandex.ru>
2022-08-12 01:20:33 +0300

    cart refacto

--- src/components/v-cart/v-cart.vue
+++ src/components/v-cart/v-cart.vue
@@ -1,40 +1,32 @@
 <template>
   <div class="v-cart">
-    <div class="title" v-if="isCartEmpty(CART)">Cart is empty</div>
     <v-cart-item
-      v-for="(cartItemValue, cartItemKey) in CART"
+      v-for="(cartItemValue, cartItemKey) in cart"
       :key="cartItemKey"
       :productArticle="cartItemKey"
       :productQuantity="cartItemValue"
-      :productData="PRODUCTS.find(el => el.article === cartItemKey)"
+      :productData="products.find(element => element.article === cartItemKey)"
     />
   </div>
 </template>
 
 <script>
 import vCartItem from '@/components/v-cart/v-cart-item.vue'
-import { mapGetters, mapActions } from 'vuex'
 
 export default {
   name: 'v-cart',
   components: {
     vCartItem
   },
-  methods: {
-    ...mapActions(['LOAD_FROM_LOCAL_STORAGE', 'GET_PRODUCTS_FROM_API']),
-    isCartEmpty(CART) {
-      return !Object.keys(CART).length
+  props: {
+    cart: {
+      type: Object,
+      require: true
     },
-    findProduct(productsList, productArticle) {
-      return productsList.find(el => el.article === productArticle)
+    products: {
+      type: Array,
+      require: true
     }
-  },
-  computed: {
-    ...mapGetters(['CART', 'PRODUCTS'])
-  },
-  mounted() {
-    this.LOAD_FROM_LOCAL_STORAGE()
-    this.GET_PRODUCTS_FROM_API(Object.keys(this.CART))
   }
 }
 </script>
--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="title">Cart</div>
-  <v-cart />
+  <div class="title" v-if="isCartEmpty(CART)">Cart is empty</div>
+  <v-cart :products="PRODUCTS" :cart="CART" />
 </template>
 
 <script>
 import vCart from '@/components/v-cart/v-cart.vue'
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
   components: {
     vCart
+  },
+  computed: {
+    ...mapGetters(['CART', 'PRODUCTS'])
+  },
+  methods: {
+    ...mapActions(['LOAD_FROM_LOCAL_STORAGE', 'GET_PRODUCTS_FROM_API']),
+    isCartEmpty(CART) {
+      return !Object.keys(CART).length
+    }
+  },
+  mounted() {
+    this.LOAD_FROM_LOCAL_STORAGE()
+    this.GET_PRODUCTS_FROM_API(Object.keys(this.CART))
   }
 }
 </script>
