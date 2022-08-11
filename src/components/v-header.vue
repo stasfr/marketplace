0f585ea7d9ed0c7195550120800df6f0322d8350
stasfr <stas240600@yandex.ru>
2022-08-12 00:48:45 +0300

    added search in header

--- src/components/v-header.vue
+++ src/components/v-header.vue
@@ -3,6 +3,10 @@
     <div class="container">
       <div class="navbar-content">
         <router-link class="navbar-logo" to="/">{{ title }}</router-link>
+        <div class="navbar-search">
+          <input class="navbar-search__input" type="text" placeholder="search...">
+          <button class="navbar-search__btn" @click="search"><font-awesome-icon icon="fa-solid fa-search" /></button>
+        </div>
         <ul class="navbar-list">
           <li class="navbar-item" v-for="link in links" :key="link.alias">
             <router-link class="navbar-link" :to="link.url">
@@ -17,12 +21,21 @@
 
 <script>
 import { links, title } from '@/_config'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 export default {
   data() {
     return {
       title,
       links
+    }
+  },
+  components: {
+    FontAwesomeIcon
+  },
+  methods: {
+    search() {
+      console.log(123);
     }
   }
 }
